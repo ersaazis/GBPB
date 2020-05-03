4b5b095cf90584008e6a575ddddff4e29b92cf35
ersaazis <eam24maret@gmail.com>
2020-05-03 21:29:06 +0700

    vnc server

--- daftarpb/run.sh
+++ daftarpb/run.sh
@@ -1,5 +1,5 @@
 Xvfb -screen 0 1020x720x16 :99 &
-x11vnc -display :99 &
+x11vnc -display :99 -forever &
 export DISPLAY=:99
 cd ~
 wget https://pjreddie.com/media/files/yolov3.weights -O /usr/local/lib/python3.7/dist-packages/goodbyecaptcha/models/yolov3.weights

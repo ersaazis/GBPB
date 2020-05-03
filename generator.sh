a5cd0dbaaadeedb8cc6a3848ec2e9293006a1b6f
ersaazis <eam24maret@gmail.com>
2020-05-03 19:09:05 +0700

    port

--- generator.sh
+++ generator.sh
@@ -5,10 +5,13 @@ echo PROXY :
 read PROXY
 echo SERVER :
 read SERVER
+echo PORT VNC :
+read PORT
 
 docker container create 
 -e PROXY = $PROXY \
 -e SERVER = $SERVER \
+-p $PORT:5900 \
 --name $name --cpus=0.9 daftarpb
 docker container start $name
 docker network connect akun_network $name

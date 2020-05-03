8b7715d007f58a2ca599fb900022c63a73379579
ersaazis <eam24maret@gmail.com>
2020-05-03 20:18:35 +0700

    installer

--- generator.sh
+++ generator.sh
@@ -8,6 +8,6 @@ read SERVER
 echo PORT VNC :
 read PORT
 
-docker container create -e PROXY = $PROXY -e SERVER = $SERVER -p $PORT:5900 --name $name --cpus=0.9 daftarpb
+docker container create -e PROXY=$PROXY -e SERVER=$SERVER -p $PORT:5900 --name $name --cpus=0.9 daftarpb
 docker container start $name
 docker network connect akun_network $name
